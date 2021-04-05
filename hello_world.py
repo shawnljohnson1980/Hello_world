9cd3e959426f8e34d599b9a80e6d995b320aed88
shawnljohnson1980 <78113657+shawnljohnson1980@users.noreply.github.com>
2021-04-05 16:24:41 -0400

    first commit

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+print("hello world")
+
+name = "Shawn"
+print(f"hello, my name is, " + (name) + "..")
+
+name = "42"
+print("Hello, " + (name))
+
+fave1 = "alfredo"
+fave2 = "cheeseburgers"
+
+print("my favorite foods are, " + (fave1), " and " + (fave2) + "!")
